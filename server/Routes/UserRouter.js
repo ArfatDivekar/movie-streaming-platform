bacdf79fbedd1b4a0622db903aaade30f0a63de1
ArfatDivekar <arfatdivekar21@gmail.com>
2024-10-24 18:03:06 +0530

    second commit

--- server/Routes/UserRouter.js
+++ server/Routes/UserRouter.js
@@ -1,5 +1,16 @@
-import express from "express"
-import { addLikedMovies, changeUserPassword, deleteLikedMovies, deleteUserProfile, deleteUser, getLikedMovies, getUsers, loginUser, registerUser, updateUserProfile } from "../Controllers/UserController.js";
+import express from "express";
+import {
+  addLikedMovies,
+  changeUserPassword,
+  deleteLikedMovies,
+  deleteUserProfile,
+  deleteUser,
+  getLikedMovies,
+  getUsers,
+  loginUser,
+  registerUser,
+  updateUserProfile,
+} from "../Controllers/UserController.js";
 import { protect, admin } from "../middlewares/Auth.js";
 
 const router = express.Router();
@@ -8,20 +19,16 @@ const router = express.Router();
 router.post("/", registerUser);
 router.post("/login", loginUser);
 
+// ********** PRIVATE ROUTES **********
+router.put("/", protect, updateUserProfile);
+router.delete("/", protect, deleteUserProfile);
+router.put("/password", protect, changeUserPassword);
+router.get("/favourites", protect, getLikedMovies);
+router.post("/favourites", protect, addLikedMovies);
+router.delete("/favourites", protect, deleteLikedMovies);
 
 // ********** PRIVATE ROUTES **********
-router.put("/", protect, updateUserProfile)
-router.delete("/", protect, deleteUserProfile)
-router.put("/password", protect, changeUserPassword)
-router.get("/favourites", protect, getLikedMovies)
-router.post("/favourites", protect, addLikedMovies)
-router.delete("/favourites", protect, deleteLikedMovies)
-
-
-// ********** PRIVATE ROUTES **********
-router.get("/", protect, admin, getUsers);
+router.get("/all", protect, admin, getUsers);
 router.delete("/:id", protect, admin, deleteUser);
-
-
 
 export default router;
