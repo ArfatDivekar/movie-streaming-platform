f3d2953d04e0bbf0089d7b6f199fdb257bd0de45
ArfatDivekar <arfatdivekar21@gmail.com>
2024-11-09 11:50:22 +0530

    Loader change

--- client/src/Components/Notifications/Loader.js
+++ client/src/Components/Notifications/Loader.js
@@ -1,11 +1,12 @@
-import { PuffLoader } from "react-spinners"
+import { PuffLoader } from "react-spinners";
 
 function Loader() {
-    return (
-        <div className="w-full py-4 px-2 flex-colo">
-            <PuffLoader color="#F20000"/>
-        </div>
-    )
+  return (
+    <div className="w-full py-4 px-2 flex-colo">
+      <PuffLoader color="#F20000" />
+      <p className="p-4">Please wait the data is loading...</p>
+    </div>
+  );
 }
 
 export default Loader;
